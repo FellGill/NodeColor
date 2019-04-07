70576047daca7364e40178a73db7fe0a5bf3d1ee
FellGill <40915285+FellGill@users.noreply.github.com>
2019-04-07 15:01:20 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+exports.name = {
+    "apricot": "#E67E30",
+    "mahogany": "#88421D",
+    "aquamarine": "#79F8F8",
+    "chestnut": "#A76726",
+    "almond": "#82C46C",
+    "amaranth": "#91283B"
+};
